fafdfa5189f135a808c1ca7442f0a79db50cfec4
LL15 <lauracallejera@gmail.com>
2017-10-17 12:43:59 +0200

    'Mas_lineas'

--- Ficheros/R/DataFrameR.R
+++ Ficheros/R/DataFrameR.R
@@ -2,3 +2,5 @@ library("XML")
 library("methods")
 result <- xmlParse(file = "C:\\resulQuery.xml")
 df <- xmlToDataFrame("C:\\resulQuery.xml")
+o_e <- df[ , "Original_Estimate"]
+t_s <- df[ , "Time_Spent"]
