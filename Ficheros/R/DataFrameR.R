c673bc3bfc57ef51c88056b17fd1f94591d728b1
LL15 <lauracallejera@gmail.com>
2017-10-18 11:35:31 +0200

    Matrix

--- Ficheros/R/DataFrameR.R
+++ Ficheros/R/DataFrameR.R
@@ -1,6 +1,15 @@
+#Carga de bibliotecas para lectura de XML
 library("XML")
 library("methods")
+#Recuperacion de los datos del XML
 result <- xmlParse(file = "C:\\resulQuery.xml")
 df <- xmlToDataFrame("C:\\resulQuery.xml")
+#Separacion de algunos datos numericos
 o_e <- df[ , "Original_Estimate"]
 t_s <- df[ , "Time_Spent"]
+#Matriz con la que se trabajara
+datos <- cbind(o_e,t_s)
+#Se cargan las bibliotecas relacionadas con
+#la clusterización
+library("cluster")
+library("fpc")
