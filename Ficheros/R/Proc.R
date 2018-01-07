17fb507014ef4b78380122ae7b52e4b2dbe15222
LL15 <lauracallejera@gmail.com>
2018-01-07 15:33:06 +0100

    KF CV

--- Ficheros/R/Proc.R
+++ Ficheros/R/Proc.R
@@ -1,7 +1,14 @@
 #Regresion con la que se hace la prediccion
 step
-#Datos para la predicion
-mydata <- cbind(data.frame(t_sT),data.frame(o_eT),data.frame(sprT))
+#Datos de entrenamieto y test
+train <- data.frame(cbind(t_sE,o_eE,sprE))
+test <- data.frame(cbind(t_sT,o_eT,sprT))
+
+#K-fold cross-validation
+library(DAAG)
+kfcv = cv.lm(train, step, m=5)
+
+#Prediccion
 x <- predict.lm(step, mydata)
 x <- as.numeric(x)
 
@@ -13,7 +20,3 @@ for(i in 1:235){
 rm(i)
 error = as.numeric(error)
 error_medio = as.numeric(error_medio)
-
-#K-fold cross-validation
-library(DAAG)
-kfcv = cv.lm(mydata, step, m=3)
